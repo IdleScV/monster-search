98ed61e8def61fbd3751d8289617fe52513edc55
Wesley Chen <weschen.github@gmail.com>
2020-01-10 21:56:48 -0500

    Updates

---
+++ precache-manifest.54aeaa1940d351bd131177fed26f4e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0233c7322560cd41774965afbe12fe9c",
+    "url": "/monster-search/index.html"
+  },
+  {
+    "revision": "804dd00af1d565e92268",
+    "url": "/monster-search/static/css/main.f00dfbf2.chunk.css"
+  },
+  {
+    "revision": "ea780a86624c6f10e78a",
+    "url": "/monster-search/static/js/2.edc771fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-search/static/js/2.edc771fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "804dd00af1d565e92268",
+    "url": "/monster-search/static/js/main.7a020ef0.chunk.js"
+  },
+  {
+    "revision": "a3b68398333d3631a7ab",
+    "url": "/monster-search/static/js/runtime-main.0c60006a.js"
+  }
+]);
